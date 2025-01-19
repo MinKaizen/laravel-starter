45e6b25f71d557bc378af5084ba0b4fab90544b1
minkaizen <martin.g.cao@gmail.com>
2025-01-19 11:18:42 +1100

    feat: install and setup folio page routing

--- resources/views/home.blade.php
+++
@@ -1,3 +0,0 @@
-@component('layouts.base')
-    <h1 class="text-3xl text-amber-600">Hello world!</h1>
-@endcomponent
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,3 @@
+@component('layouts.base')
+    <h1 class="text-3xl text-amber-600">Hello from folio!</h1>
+@endcomponent
