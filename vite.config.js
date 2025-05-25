b6005e7649cd6eb0ae2022fe0b5d115fbf0284ff
minkaizen <martin.g.cao@gmail.com>
2025-05-26 08:27:41 +1000

    fix: vite and postcss build process

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,29 @@ import path from 'path'
 process.env = { ...process.env, ...loadEnv('', process.cwd()) };
 const appDomain = new URL(process.env.VITE_APP_URL).hostname;
 
-export default defineConfig({
-    server: {
-      host: '0.0.0.0',
-      hmr: {
-          host: appDomain,
-      },
-      https: {
-        key: fs.readFileSync(path.join(process.env.VITE_CERTS_DIR, appDomain) + '-key.pem'),
-        cert: fs.readFileSync(path.join(process.env.VITE_CERTS_DIR, appDomain) + '.pem'),
-      },
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const isDevelopment = mode === "development";
+
+    return {
+        ...(isDevelopment
+            ? {
+                server: {
+                    host: '0.0.0.0',
+                    hmr: {
+                        host: appDomain,
+                    },
+                    https: {
+                        key: fs.readFileSync(path.join(process.env.VITE_CERTS_DIR, appDomain) + '-key.pem'),
+                        cert: fs.readFileSync(path.join(process.env.VITE_CERTS_DIR, appDomain) + '.pem'),
+                    },
+                },
+            }
+            : {}),
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    }
 });
